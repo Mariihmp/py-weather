b99a1965c08d8f0a3d7bdc1a76963285e7f9c397
marhmpa <marhmpa@gmail.com>
2023-09-22 11:24:14 +0330

    first commit

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, redirect, render_template, request, url_for
+from weather import get_current_weather
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/weather')
+def get_weather():
+    city = request.args.get('city')
+    if not bool(city.strip()):
+        city = "London"
+    weather_data = get_current_weather(city)
+    if not weather_data['cod'] == 200:
+        # return redirect(url_for('index'))
+        return redirect(url_for('index'))
+    return render_template('weather.html', title=weather_data['name'], status=weather_data['weather'][0]['description'].capitalize(),          temp=weather_data['main']['temp'], feels_like=weather_data['main']['feels_like']
+                           )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ weather.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+city = "Tehran"
+
+
+def get_current_weather(city="Tehran"):
+    requests_url = f"https://api.openweathermap.org/data/2.5/weather?&appid={os.getenv('API_KEY')}&q={city}&units=imperial"
+
+    weather_data = requests.get(requests_url).json()
+    return weather_data
+
+
+if __name__ == "__main__":
+    print('\n*** ger weather condition***:')
+    city = input('\n please enter a city name')
+
+    if not bool(city.strip()):
+        city = "London"
+    weather_data = get_current_weather(city)
+
+    print('\n')
+    pprint(weather_data)
